d074b0ac1d28dd54f9643218b412d5f21f81962a
Nicolás Massardo <nmassardot@uc.cl>
2022-06-26 14:00:52 -0400

    feat(following): display users

--- src/components/SectionFollowers.jsx
+++ src/components/SectionFollowers.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-import { getFollowers } from "../api/github";
+import clsx from "clsx";
 
-import FollowersDisplay from "./specifics/FollowersDisplay";
+import { getFollowers, getFollowing } from "../api/github";
+import UsersDisplay from "./specifics/UsersDisplay";
 
 function SectionFollowers({ username }) {
   const [followers, setFollowers] = useState([]);
+  const [following, setFollowing] = useState([]);
 
   useEffect(() => {
     const fetchFollowers = async () => {
@@ -14,9 +16,18 @@ function SectionFollowers({ username }) {
     };
     fetchFollowers();
   }, [username]);
+
+  useEffect(() => {
+    const fetchFollowedBy = async () => {
+      const data = await getFollowing(username);
+      setFollowing(data);
+    }
+    fetchFollowedBy();
+  }, [username])
   return (
-    <div>
-      <FollowersDisplay followers={followers} />
+    <div className={clsx("h-full w-full", "flex items-center justify-center", "py-10")}>
+      <UsersDisplay users={followers} className={clsx("mr-10")} />
+      <UsersDisplay users={following} />
     </div>
   );
 }
