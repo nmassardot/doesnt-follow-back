c314df68606550bbc8b1f96af51185f16b07d429
Nicolás Massardo <nmassardot@uc.cl>
2022-06-26 15:33:52 -0400

    feat(footer): add v1

--- src/components/SectionFollowers.jsx
+++ src/components/SectionFollowers.jsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 
 import { getFollowers, getFollowing, getUsersStarredRepo } from "../api/github";
 import UsersDisplay from "./specifics/UsersDisplay";
+import Footer from "./specifics/Footer";
 
 function orderByUsername(follower1, follower2) {
   const follower1UsernameLowerCase = follower1.login.toLowerCase();
@@ -57,42 +58,45 @@ function SectionFollowers({ username }) {
   }, [followers, following, username]);
 
   return (
-    <div
-      className={clsx(
-        "h-full w-full",
-        "flex",
-        !starredRepo && "flex-col",
-        "items-center justify-center",
-        "py-10"
-      )}
-    >
-      {starredRepo && (
-        <>
-          <UsersDisplay users={followers} className={clsx("mr-10")} />
-          <UsersDisplay users={following} className={clsx("mr-10")} />
-          <UsersDisplay users={doesntFollowBack} />
-        </>
-      )}
-      {!starredRepo && (
-        <>
-          <h1
-            className={clsx("text-3xl font-bold", "mb-5")}
-          >
-            Remember to star the repository to see whom doesn't follow you back!
-          </h1>
-          <p className={clsx("text-xl")}>You can give it a star in {" "}
-            <a
-              className={clsx("text-blue-700 hover:text-blue-500", "underline cursor-pointer")}
-              href="https://github.com/nmassardot/doesnt-follow-back"
-              target="_blank"
-              rel="noreferrer"
+    <>
+      <div
+        className={clsx(
+          "h-full w-full",
+          "flex",
+          !starredRepo && "flex-col",
+          "items-center justify-center",
+          "py-10"
+        )}
+      >
+        {starredRepo && (
+          <>
+            <UsersDisplay users={followers} className={clsx("mr-10")} />
+            <UsersDisplay users={following} className={clsx("mr-10")} />
+            <UsersDisplay users={doesntFollowBack} />
+          </>
+        )}
+        {!starredRepo && (
+          <>
+            <h1
+              className={clsx("text-3xl font-bold", "mb-5")}
             >
-              here
-            </a>
-          </p>
-        </>
-      )}
-    </div>
+              Remember to star the repository to see whom doesn't follow you back!
+            </h1>
+            <p className={clsx("text-xl")}>You can give it a star in {" "}
+              <a
+                className={clsx("text-blue-700 hover:text-blue-500", "underline cursor-pointer")}
+                href="https://github.com/nmassardot/doesnt-follow-back"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+            </p>
+          </>
+        )}
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/specifics/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import clsx from "clsx";
+
+function Footer() {
+  return (
+    <div
+      className={clsx(
+        "fixed bottom-0",
+        "flex justify-center items-center",
+        "w-full bg-gray-100",
+        "py-4",
+        "text-md",
+      )}
+    >
+      <p>Made with ❤️  by {" "}
+      <a
+        href="https://github.com/nmassardot"
+        className={clsx("font-bold hover:underline")}
+      >
+        nmassardot
+      </a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
