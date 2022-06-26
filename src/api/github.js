a2ffbd3aa6a2152534fdbd0f4bbc284afccba944
Nicolás Massardo <nmassardot@uc.cl>
2022-06-26 13:21:03 -0400

    fix(api/github): while condition

--- src/api/github.js
+++ src/api/github.js
@@ -2,16 +2,16 @@ import { githubClient } from "./index";
 
 export async function getFollowers(username) {
   try {
-    const res = await githubClient.get(`/users/${username}/followers`);
+    const res = await githubClient.get(`/users/${username}/followers?per_page=100`);
 
-    if (res && res.data.length < 30) {
+    if (res && res.data.length < 100) {
       return res.data;
     } else {
       const allFollowers = [];
       allFollowers.push(...res.data);
       let pageNumber = 2;
-      while (res !== [] && pageNumber < 10) {
-        const res = await githubClient.get(`/users/${username}/followers?page=${pageNumber}`);
+      while (res.data.length === 100) {
+        const res = await githubClient.get(`/users/${username}/followers?page=${pageNumber}&per_page=100`);
         allFollowers.push(...res.data);
         pageNumber++;
       }
