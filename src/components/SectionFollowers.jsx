8e9c51ede54e16ccc97832448d8f3b1eadf84da2
Nicolás Massardo <nmassardot@uc.cl>
2022-06-26 14:23:03 -0400

    feat(doesnt-follow-back): calculate users

--- src/components/SectionFollowers.jsx
+++ src/components/SectionFollowers.jsx
@@ -8,6 +8,7 @@ import UsersDisplay from "./specifics/UsersDisplay";
 function SectionFollowers({ username }) {
   const [followers, setFollowers] = useState([]);
   const [following, setFollowing] = useState([]);
+  const [doesntFollowBack, setDoesntFollowBack] = useState([]);
 
   useEffect(() => {
     const fetchFollowers = async () => {
@@ -23,11 +24,26 @@ function SectionFollowers({ username }) {
       setFollowing(data);
     }
     fetchFollowedBy();
-  }, [username])
+  }, [username]);
+
+  useEffect(() => {
+    const followersUsernames = followers.map((f) => f.login);
+    const doesntFollow = [];
+
+    following.forEach((f) => {
+      console.log(!followersUsernames.includes(f.login));
+      if (!(followersUsernames.includes(f.login)) && !(doesntFollow.includes(f))) {
+        doesntFollow.push(f);
+      }
+    });
+
+    setDoesntFollowBack(doesntFollow);
+  }, [followers, following]);
   return (
     <div className={clsx("h-full w-full", "flex items-center justify-center", "py-10")}>
       <UsersDisplay users={followers} className={clsx("mr-10")} />
-      <UsersDisplay users={following} />
+      <UsersDisplay users={following} className={clsx("mr-10")} />
+      <UsersDisplay users={doesntFollowBack} />
     </div>
   );
 }
