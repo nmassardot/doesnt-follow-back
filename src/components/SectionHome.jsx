db6ab0e1cd8cbcdeb4d632088201f02b85a4b365
Nicolás Massardo <nmassardot@uc.cl>
2022-06-27 12:45:06 -0400

    refactor(home): improve text

--- src/components/SectionHome.jsx
+++ src/components/SectionHome.jsx
@@ -41,9 +41,9 @@ function SectionHome() {
             Who doesn't follow you back?
           </h1>
           <p className={clsx("mb-12")}>
-            There are people that you follow that just don't
-            want to follow you back on Github. Here you have de oportunity to
-            know whom they are, and if it's still woth it to follow them.
+            There are people that you follow on Github that just don't
+            want to follow you back. Here you have de oportunity to
+            know whom they are, and if it's still worth it to follow them.
           </p>
           <div className={clsx("flex items-center justify-center", "w-full", "md:px-10", "mb-12")}>
             <input
@@ -69,8 +69,8 @@ function SectionHome() {
             </button>
           </div>
           <p>
-            In the best case, you can tell them to follow you back, and,
-            if they do, no hard feelings and all it's ok
+            In the best case scenario, you can tell them to follow you back, and,
+            if they do, no hard feelings and all it's ok.
           </p>
       </div>
       <Footer />
