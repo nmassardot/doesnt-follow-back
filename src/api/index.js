a62e85b1e4b6cb179c0a4ee2561883330e964a9b
Jose Antonio Castro <jacastro18@uc.cl>
2022-06-29 23:12:39 -0400

    fix(eslint): ignore rule

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const GITHUB_API_URL = "https://github-followers-api.herokuapp.com";
 const GITHUB_API_URL = process.env.REACT_APP_CUSTOM_GITHUB_API_URL;
-
+/* eslint-disable import/prefer-default-export */
 export const githubClient = axios.create({
   baseURL: GITHUB_API_URL,
 });
+/* eslint-enable import/prefer-default-export */
