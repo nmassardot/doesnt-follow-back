cfbc80d36066add9ce88d196640c9c3dc5ce3f2b
Jose Antonio Castro <jacastro18@uc.cl>
2022-06-29 23:11:46 -0400

    fix(eslint-test): both passing in App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+/* eslint-disable no-undef */
+test('renders home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Who doesn't follow you back/i);
   expect(linkElement).toBeInTheDocument();
 });
+/* eslint-enable no-undef */
